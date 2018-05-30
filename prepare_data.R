bb7c8f82411f2efb9cc12d3d00460aa41279252c
PW <pwasows@gmail.com>
2018-05-30 18:17:24 +0200

    Fix typos and add stopwords removal

--- prepare_data.R
+++ prepare_data.R
@@ -1,29 +1,39 @@
-require("tm")
-require("tm.plugin.mail")
-require(tidytext)
+if (! "tm" %in% row.names(installed.packages()))
+  install.packages("tm")
+library("tm")
+if (! "tm.plugin.mail" %in% row.names(installed.packages()))
+  install.packages("tm.plugin.mail")
+library("tm.plugin.mail")
+if (! "tidytext" %in% row.names(installed.packages()))
+  install.packages("tidytext")
+library("tidytext")
+
 
 prepare_corpus <- function(corpus) {
   corpus <- tm_map(corpus, removeMultipart)
+  #necessary?
   corpus <- tm_map(corpus, content_transformer(function(x) iconv(x, to = "UTF-8", sub = "byte")))
+  #apart from stripWhitespace, all the operations below may be replaced with specifying proper options in control list passed to DocumentTermMatrix()
   corpus <- tm_map(corpus, content_transformer(tolower))
   corpus <- tm_map(corpus, stripWhitespace)
+  corpus <- tm_map(corpus, removeWords, stopwords("english"))
   corpus <- tm_map(corpus, removeNumbers)
   corpus <- tm_map(corpus, stemDocument)
   corpus <- tm_map(corpus, removePunctuation)
   return(corpus)
 }
 
-
-
 non_spam <-  Corpus(DirSource("easy_ham", encoding = "UTF-8"), readerControl = list(reader = readMail, language="en"))
 non_spam <- prepare_corpus(non_spam)
-non_spam.TF <- DocumentTermMatrix(non_spam, control = list(weighting = weightTf, minDocFreq=3))
-non_spam.TFIDF <- DocumentTermMatrix(non_spam, control = list(weigthing = weightTfIdf, minDocFreq=3))
+non_spam.TFIDF <- DocumentTermMatrix(non_spam, control = list(weighting = weightTfIdf))
+non_spam.BIN <- DocumentTermMatrix(non_spam, control = list(weighting = weightBin))
 
 spam <- Corpus(DirSource("spam"), readerControl = list(reader = readMail, language="en"))
+#spam[[38]] causes the error - it contains invalid utf8 characters
+
 spam <- prepare_corpus(spam)
-spam.TF <- DocumentTermMatrix(spam, control = list(weigthing = weightTf, minDocFreq=3))
-spam.TFIDF <- DocumentTermMatrix(spam, control = list(weigthing = weightTfIdf, minDocFreq=3))
+spam.TF <- DocumentTermMatrix(spam, control = list(weighting = weightTf))
+spam.TFIDF <- DocumentTermMatrix(spam, control = list(weighting = weightTfIdf))
 
 # converting to DT Data Frame:
 non_spam.TFIDF.df <- tidy(non_spam.TFIDF)
